36f0683bdc4c16e590f7179e5f686ede11079158
Jaliel <kangjaliel1998@gmail.com>
2024-02-10 16:08:39 +0700

    Initial Commit

---
+++ FindYourWaifu/FindYourWaifuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindYourWaifuApp.swift
+//  FindYourWaifu
+//
+//  Created by Jaliel on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FindYourWaifuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
